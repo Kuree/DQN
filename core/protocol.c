adf4ed850d9b7df61301643f08b0312714ee7f39
Keyi Zhang <kz005@bucknell.edu>
2017-01-22 21:47:58 -0500

    added some initial protocol design details

---
+++ core/protocol.c
@@ -0,0 +1,21 @@
+#define BLOOM_SIZE 64
+
+uint64_t bloom_hash(uint64_t hash_value, int value){
+    uint64_t hash_value_temp = hash(value);
+    return hash_value | (1 << (hash_value_temp % BLOOM_SIZE));	
+}
+
+
+bool bloom_test(uint64_t hash_value, int value){
+    uint64_t hash_value_temp = hash(value);
+    int bit_pos = hash_value_temp % BLOOM_SIZE;
+    return !(!(hash_value & (1 << bit_pos)))
+}
+
+// taken from http://stackoverflow.com/a/12996028
+uint64_t hash(uint64_t x) {
+    x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
+    x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
+    x = x ^ (x >> 31);
+    return x;
+}
--- core/protocol.h
+++ core/protocol.h
@@ -1,8 +1,31 @@
-struct TR{
+struct dqn_tr{
  	// 2 bytes
-	uint16_t 	id;
+	uint16_t 	    id;
 	// 1 byte
-	uint8_t  	num_slots;
+    uint8_t  	    num_slots;
     // 1 byte
-	uint8_t		crc
+	uint8_t		    crc
 } __attribute__((packed));  // total is 4 bytes
+
+
+struct dqn_data{
+    // 30 bytes
+    uint8_t[30]     data
+} __attribute__((packed));  // total is 30 bytes
+
+
+struct feedback{
+    // 2 bytes
+    uint16_t        dtq_length;
+    // 2 bytes
+    uint16_t        crq_length;
+    // 8 bytes
+    uint64_t        result; // this is bloom filter result
+} __attribute__((packed));  // total is 12 bytes
+
+
+// this is used to hash an integer value into
+// to create a new bloom filter, pass 0 as the hash_value to initialize it
+uint64_t bloom_hash(uint64_t hash_value, int value);
+
+bool bloom_test(int value);

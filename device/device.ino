d48b0f552545980956c2623ca9d03699ec21d6c3
Keyi Zhang <kz005@bucknell.edu>
2017-01-26 19:04:44 -0500

    added arduino setup

--- device/device.ino
+++ device/device.ino
@@ -1,11 +1,57 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <protocol.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#define RF95_FREQ 915.0
+
+#define VBATPIN A7
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT); // Adafruit Feather M0 with RFM95 
+
 
 void setup() {
-        
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
+    pinMode(VBATPIN, INPUT);
+
+    // un-reset the radio
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+
+    Serial.begin(57600);
+    while (!Serial) ; // Wait for serial port to be available (does not boot headless!)
+    if (!rf95.init()){
+        Serial.println("init failed");
+
+        while (1);
+    }
+    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+    if (!rf95.setFrequency(RF95_FREQ)) {
+        Serial.println("setFrequency failed"); 
+        while (1);
+    }
+    Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+    // The default transmitter power is 13dBm, using PA_BOOST.
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+    // you can set transmitter powers from 5 to 23 dBm:
+    rf95.setTxPower(23, false);
+
+    if (rf95.setModemConfig(rf95.Bw500Cr45Sf128)){
+        Serial.println("rf95 configuration set to BW=500 kHz BW, CR=4/5 CR, SF=7.");
+    } else{
+        Serial.println("rf95 configuration failed.");
+        while (1);
+    }
+
 }
 
 void loop() {
-        
+
 }
 
